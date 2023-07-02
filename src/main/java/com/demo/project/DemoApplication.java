ecdeb9d3781adfcbfd2e62a64dc01c739539129f
Akshay Meshram <akshay.d7769@gmail.com>
2023-07-02 11:26:15 +0530

    feat/added-branch-message

--- src/main/java/com/demo/project/DemoApplication.java
+++ src/main/java/com/demo/project/DemoApplication.java
@@ -8,6 +8,9 @@ public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+		System.out.println("I am in main method");
+
+		System.out.println("Hello I am in Akshay's branch");
 	}
 
 }
